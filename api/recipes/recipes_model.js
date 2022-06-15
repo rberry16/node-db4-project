4251c8f4a51b86490d83ded6841672b12e5cfec2
Ryan Berry <rberry16@hotmail.com>
2022-06-15 11:26:37 -0700

    debugger made

---
+++ api/recipes/recipes_model.js
@@ -0,0 +1,5 @@
+const get = () => {
+    return 'this is where the fun begins!'
+}
+
+module.exports = {get};
--- api/recipes/recipes_router.js
+++ api/recipes/recipes_router.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
+const Rec = require('./recipes_model');
 
-router.use('*', (req, res, next) => {
-    res.json(`it's working! it's working!`);
+router.use('*', async (req, res) => {
+    const th = await Rec.get();
+    res.json(th);
+})
+
+router.use((err, req, res, next) => {//eslint-disable-line
+    res.status(500).json({
+        customMessage: `houston we have a problem`,
+        message: err.message,
+        stack: err.stack
+    })
 })
 
 module.exports = router;
